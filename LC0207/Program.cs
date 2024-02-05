a850ae0d81a69cab9b51acb10168fd0adf5981db
Alexandra Camarena <alexandracamarena@ou.edu>
2024-02-05 16:28:14 -0600

    Add project files.

---
+++ LC0207/Program.cs
@@ -0,0 +1,22 @@
+﻿// MIS 3013 001
+// Feb 7, 2024
+// Alexandra Camarena
+// 113568154
+
+// for loop
+// for (0;1;2)
+//{
+//      3;
+//}
+// 0: initalization
+// 1: conditional (true or false)
+// 3: loop body
+// 2: post processing
+// 0 ->1->3->2->
+//      1->3->2->
+
+for (int i = 20; i <= 40; i = i + 1)
+{
+    // loop body
+    Console.WriteLine(i);
+}
